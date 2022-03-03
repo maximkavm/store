844f4416a0ae020ada4daf4f58592277a8328d7f
Maxim Meschankin <maxikavm@gmail.com>
2022-03-03 10:19:30 +0500

    20322

--- products/views.py
+++ products/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'products/index.html')
 
 
-
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Test title',
+        'products':[
+            {'name': 'Худи черногоцвета цвета', 'price': 1050},
+            {'name': 'Джинсы', 'price': 750},
+            {'name': 'Футболка', 'price': 105},
+
+        ]
+    }
+    return render(request, 'products/products.html', context)
