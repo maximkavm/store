7bae3c5edeabea2e15166e8008b5d1f7bbd0c793
Maxim Meschankin <maxikavm@gmail.com>
2022-03-03 10:24:27 +0500

    30322

--- products/views.py
+++ products/views.py
@@ -9,10 +9,22 @@ def index(request):
 def products(request):
     context = {
         'title': 'Test title',
-        'products':[
-            {'name': 'Худи черногоцвета цвета', 'price': 1050},
-            {'name': 'Джинсы', 'price': 750},
-            {'name': 'Футболка', 'price': 105},
+        'products': [
+            {
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+                'price': 6090
+            },
+            {
+                'name': 'Синяя куртка The North Face',
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+                'price': 23725
+            },
+            {
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+                'price': 3390            },
+
 
         ]
     }
